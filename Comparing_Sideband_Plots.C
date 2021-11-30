604e4c4cc05900c3fa43c944e1873d3b12b392d7
mathewnicol15 <matthew.nicol@york.ac.uk>
2021-11-30 11:32:28 +0000

    Normalising the histograms and improving appearance

--- Comparing_Sideband_Plots.C
+++ Comparing_Sideband_Plots.C
@@ -7,7 +7,28 @@
   TH1F *RGA_missing_mass = (TH1F*)RGA_Data->Get("h_projectionx_S2_sig_1");
 TH1F *RGA_smeared_missing_mass = (TH1F*)RGA_Data_Smeared->Get("h_projectionx_S2_sig_1");
 
-RGB_missing_mass->Draw();
-RGA_missing_mass->Draw("same");
+RGB_missing_mass->Rebin(2);
+RGA_missing_mass->Rebin(2);
+RGA_smeared_missing_mass->Rebin(2);
+
+Double_t RGA_RGB_Scale = RGA_smeared_missing_mass->Integral() / RGB_missing_mass->Integral();
+Double_t RGA_RGA_smeared = RGA_smeared_missing_mass->Integral() / RGA_missing_mass->Integral();
+
+RGB_missing_mass->Scale(RGA_RGB_Scale);
+RGA_missing_mass->Scale(RGA_RGA_smeared);
+
+// RGB_missing_mass->SetLineColor(kRed);
+// RGB_missing_mass->SetMarkerColor(kRed);
+
+auto *c1 = new TCanvas("c1","RGB and smeared RGA",800,800);
+c1->cd();
+RGB_missing_mass->SetTitle("RGB and smeared RGA");
+RGB_missing_mass->Draw("hist,L");
 RGA_smeared_missing_mass->Draw("same");
+
+auto *c2 = new TCanvas("c2","RGA and smeared RGA",800,800);
+c2->cd();
+RGA_smeared_missing_mass->SetTitle("RGA and smeared RGA");
+RGA_smeared_missing_mass->Draw();
+RGA_missing_mass->Draw("same, hist, L");
 }
