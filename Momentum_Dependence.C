5aa105ba73222d305ffc64e1c5c557b1ecc40fce
mathewnicol15 <matthew.nicol@york.ac.uk>
2022-03-16 15:17:06 +0000

    Removed unecessary lines

--- Momentum_Dependence.C
+++ Momentum_Dependence.C
@@ -1,121 +1,9 @@
-// {
-//
-//   TFile *f1 = new TFile("/media/mn688/Elements1/PhD/Analysis_Output/Hexaquark/RGA_Fall2018_Outbending_at_least_1e1KpFD_Tree_Total_24022022_Total_Scaling_09032022_02.root");
-//   // TFile *f1 = new TFile("/media/mn688/Elements1/PhD/Analysis_Output/Hexaquark/RGA_Fall2018_Outbending_at_least_1e1KpFD_Tree_Total_24022022_Total_Scaling_09032022_02.root");
-//   TH3F* hist=(TH3F*)f1->Get("h_S1_Kaon_Momentum__Miss_Mass__Kaon_Mass");
-//
-//   TH1F* Missing_Mass_1 = (TH1F*) hist->ProjectionY("",hist->GetXaxis()->FindBin(0.5),hist->GetXaxis()->FindBin(1.0),0,hist->GetNbinsZ())->Clone("Missing_Mass_1");
-//   TH1F* Missing_Mass_2 = (TH1F*) hist->ProjectionY("",hist->GetXaxis()->FindBin(1.0),hist->GetXaxis()->FindBin(1.5),0,hist->GetNbinsZ())->Clone("Missing_Mass_2");
-//   TH1F* Missing_Mass_3 = (TH1F*) hist->ProjectionY("",hist->GetXaxis()->FindBin(1.5),hist->GetXaxis()->FindBin(2.0),0,hist->GetNbinsZ())->Clone("Missing_Mass_3");
-//   TH1F* Missing_Mass_4 = (TH1F*) hist->ProjectionY("",hist->GetXaxis()->FindBin(2.0),hist->GetXaxis()->FindBin(2.5),0,hist->GetNbinsZ())->Clone("Missing_Mass_4");
-//   TH1F* Missing_Mass_5 = (TH1F*) hist->ProjectionY("",hist->GetXaxis()->FindBin(2.5),hist->GetXaxis()->FindBin(3.0),0,hist->GetNbinsZ())->Clone("Missing_Mass_5");
-//
-//   TH1F* Kaon_Mass_1 = (TH1F*) hist->ProjectionZ("",hist->GetXaxis()->FindBin(0.5),hist->GetXaxis()->FindBin(1.0),0,hist->GetNbinsY())->Clone("Kaon_Mass_1");
-//   TH1F* Kaon_Mass_2 = (TH1F*) hist->ProjectionZ("",hist->GetXaxis()->FindBin(1.0),hist->GetXaxis()->FindBin(1.5),0,hist->GetNbinsY())->Clone("Kaon_Mass_2");
-//   TH1F* Kaon_Mass_3 = (TH1F*) hist->ProjectionZ("",hist->GetXaxis()->FindBin(1.5),hist->GetXaxis()->FindBin(2.0),0,hist->GetNbinsY())->Clone("Kaon_Mass_3");
-//   TH1F* Kaon_Mass_4 = (TH1F*) hist->ProjectionZ("",hist->GetXaxis()->FindBin(2.0),hist->GetXaxis()->FindBin(2.5),0,hist->GetNbinsY())->Clone("Kaon_Mass_4");
-//   TH1F* Kaon_Mass_5 = (TH1F*) hist->ProjectionZ("",hist->GetXaxis()->FindBin(2.5),hist->GetXaxis()->FindBin(3.0),0,hist->GetNbinsY())->Clone("Kaon_Mass_5");
-//
-//   Double_t Missing_Mass_1_integral = Missing_Mass_1->Integral();
-//   Double_t Missing_Mass_2_integral = Missing_Mass_2->Integral();
-//   Double_t Missing_Mass_3_integral = Missing_Mass_3->Integral();
-//   Double_t Missing_Mass_4_integral = Missing_Mass_4->Integral();
-//   Double_t Missing_Mass_5_integral = Missing_Mass_5->Integral();
-//
-//   Double_t Kaon_Mass_1_integral = Kaon_Mass_1->Integral();
-//   Double_t Kaon_Mass_2_integral = Kaon_Mass_2->Integral();
-//   Double_t Kaon_Mass_3_integral = Kaon_Mass_3->Integral();
-//   Double_t Kaon_Mass_4_integral = Kaon_Mass_4->Integral();
-//   Double_t Kaon_Mass_5_integral = Kaon_Mass_5->Integral();
-//
-//   Missing_Mass_2->Scale(Missing_Mass_1_integral / Missing_Mass_2_integral);
-//   Missing_Mass_3->Scale(Missing_Mass_1_integral / Missing_Mass_3_integral);
-//   Missing_Mass_4->Scale(Missing_Mass_1_integral / Missing_Mass_4_integral);
-//   Missing_Mass_5->Scale(Missing_Mass_1_integral / Missing_Mass_5_integral);
-//
-//
-//   Kaon_Mass_2->Scale(Kaon_Mass_1_integral / Kaon_Mass_2_integral);
-//   Kaon_Mass_3->Scale(Kaon_Mass_1_integral / Kaon_Mass_3_integral);
-//   Kaon_Mass_4->Scale(Kaon_Mass_1_integral / Kaon_Mass_4_integral);
-//   Kaon_Mass_5->Scale(Kaon_Mass_1_integral / Kaon_Mass_5_integral);
-//
-//
-//   Missing_Mass_2->SetLineColor(kRed);
-//   Missing_Mass_3->SetLineColor(kOrange);
-//   Missing_Mass_4->SetLineColor(kBlack);
-//   Missing_Mass_5->SetLineColor(kGreen);
-//
-//   Kaon_Mass_2->SetLineColor(kRed);
-//   Kaon_Mass_3->SetLineColor(kOrange);
-//   Kaon_Mass_4->SetLineColor(kBlack);
-//   Kaon_Mass_5->SetLineColor(kGreen);
-//
-//
-//   TF1 *func1 = new TF1(funcname.str().c_str(),"gaus(0)+gaus(3)+pol3(6)",0.365,0.6);
-//   TF1 *func2 = new TF1("func2","gaus(0)+gaus(3)",0.365,0.6);
-//   TF1 *func3 = new TF1("func3","pol3(0)",0.365,0.6);
-//   func1[i]->SetParameter(0,Kaon_Mass_3->GetMaximum()/2);
-//   func1[i]->SetParameter(1,0.493);
-//   func1[i]->SetParameter(2,0.03);
-//   func1[i]->SetParameter(3,Kaon_Mass_3->GetMaximum()/3);
-//   func1[i]->SetParameter(4,0.493);
-//   func1[i]->SetParameter(5,0.05);
-//   // func1[i]->SetParameter(5,0.05);
-//   // func1[i]->SetParameter(5,0.05);
-//   // func1[i]->SetParameter(5,0.05);
-//   // func1[i]->SetParameter(5,0.05);
-//
-//   Kaon_Mass_3->Fit(funcname.str().c_str(),"RB");
-//
-//   func2->FixParameter(0,func1[i]->GetParameter(0));
-//   func2->FixParameter(1,func1[i]->GetParameter(1));
-//   func2->FixParameter(2,func1[i]->GetParameter(2));
-//   func2->FixParameter(3,func1[i]->GetParameter(3));
-//   func2->FixParameter(4,func1[i]->GetParameter(4));
-//   func2->FixParameter(5,func1[i]->GetParameter(5));
-//   func3->FixParameter(0,func1[i]->GetParameter(6));
-//   func3->FixParameter(1,func1[i]->GetParameter(7));
-//   func3->FixParameter(2,func1[i]->GetParameter(8));
-//   func3->FixParameter(3,func1[i]->GetParameter(9));
-//   // func3->FixParameter(4,func1[i]->GetParameter(10));
-//
-//   func1[i]->SetLineColor(kBlue);
-//   func2->SetLineColor(kBlack);
-//   func3->SetLineColor(kGreen);
-//
-//
-//
-//   auto *c1 = new TCanvas("c1","",800,800);
-//   c1->cd();
-//   Missing_Mass_1->Draw("same");
-//   Missing_Mass_2->Draw("same,hist");
-//   Missing_Mass_3->Draw("same,hist");
-//   Missing_Mass_4->Draw("same,hist");
-//   Missing_Mass_5->Draw("same,hist");
-//
-//   auto *c2=new TCanvas("c2","",800,800);
-//   c2->cd();
-//   Kaon_Mass_1->Draw("same");
-//   Kaon_Mass_2->Draw("same,hist");
-//   Kaon_Mass_3->Draw("same,hist");
-//   Kaon_Mass_4->Draw("same,hist");
-//   Kaon_Mass_5->Draw("same,hist");
-//
-//   auto *c3 = new TCanvas("c3","",800,800);
-//   c3->cd();
-//   Kaon_Mass_3->Draw("same,hist");
-//   func1[i]->Draw("same");
-//   func2->Draw("same");
-//   func3->Draw("same");
-//
-// }
-
 // Making lots of projections in momentum
 
 {
-  TFile *f1 = new TFile("/media/mn688/Elements1/PhD/Analysis_Output/Hexaquark/RGA_Fall2018_Outbending_at_least_1e1KpFD_Tree_Total_24022022_Total_Scaling_10032022_01.root");
+  TFile *f1 = new TFile("/media/mn688/Elements1/PhD/Analysis_Output/Hexaquark/RGA_Fall2018_Outbending_at_least_1e1KpFD_Tree_Total_24022022_Total_Scaling_16032022_01.root");
   // TFile *f1 = new TFile("/media/mn688/Elements1/PhD/Analysis_Output/Hexaquark/RGA_Fall2018_Outbending_at_least_1e1KpFD_Tree_Total_216022022_Total_Scaling_09032022_02.root");
-  TH3F* hist2=(TH3F*)f1->Get("h_S1_Kaon_Momentum__Miss_Mass__Kaon_Mass");
+  TH3F* hist2=(TH3F*)f1->Get("h_S3_Kaon_Momentum__Miss_Mass__Kaon_Mass");
 
   TH1F *h_sigma_1 = new TH1F("h_sigma_1","sigma 1 relationship",160,1,161);
   TH1F *h_sigma_2 = new TH1F("h_sigma_2","sigma 2 relationship",160,1,161);
@@ -150,6 +38,7 @@
      bin_1 = 1 + (i*0.01);
      bin_2 = 1 + (i+1)*0.01;
 
+
      // Cutting out pion cross over between 1.1 and 1.35 GeV
      if(bin_1 > 1.1 && bin_1 < 1.35) continue;
 
@@ -159,6 +48,8 @@
 
      Missing_Mass_Projections[i] = (TH1F*) hist2->ProjectionY("",hist2->GetXaxis()->FindBin(bin_1),hist2->GetXaxis()->FindBin(bin_2),0,hist2->GetNbinsZ())->Clone();
      Kaon_Mass_Projections[i] = (TH1F*) hist2->ProjectionZ("",hist2->GetXaxis()->FindBin(bin_1),hist2->GetXaxis()->FindBin(bin_2),0,hist2->GetNbinsY())->Clone();
+
+     Kaon_Mass_Projections[i]->Rebin(2);
 
      // Setting range of functions
      ostringstream funcname, sig1, sig2, back1, back2;
