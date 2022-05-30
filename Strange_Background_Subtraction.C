fa4182e51b31a2675af910ea92587bc0133a4b58
mathewnicol15 <matthew.nicol@york.ac.uk>
2022-05-30 11:21:57 +0100

    Added S3 and adjusted normalisation. Working before playing with subtraction

--- Strange_Background_Subtraction.C
+++ Strange_Background_Subtraction.C
@@ -7,12 +7,17 @@
 
    // Input file
    // RGA data
-   TFile *f1=new TFile("/media/mn688/Elements1/PhD/Analysis_Output/Hexaquark/RGA_Fall2018_Outbending_at_least_1e1KpFD_Tree_Total_24022022_Total_Scaling_10032022_01.root");
+   TFile *f1=new TFile("/media/mn688/Elements1/PhD/Analysis_Output/Hexaquark/RGB_Spring2019_Inbending_at_least_1e1KpFD_Tree_Total_03032022_Total_Scaling_23032022_01.root");
 
    // Getting the multidimensional histogram plots
    TH3F *h_S1_Photon_Energy__Miss_Mass__Kaon_Mass = (TH3F*)f1->Get("h_S1_Photon_Energy__Miss_Mass__Kaon_Mass");
    TH3F *h_S2_Photon_Energy__Miss_Mass__Kaon_Mass = (TH3F*)f1->Get("h_S2_Photon_Energy__Miss_Mass__Kaon_Mass");
    TH3F *h_S3_Photon_Energy__Miss_Mass__Kaon_Mass = (TH3F*)f1->Get("h_S3_Photon_Energy__Miss_Mass__Kaon_Mass");
+
+
+   TH1D *S3_photon_energy_total = new TH1D("S3_photon_energy_total","",120,0,12);
+   TH1D *S3_miss_mass_total = new TH1D("S3_miss_mass_total","",300,0,3);
+   TH1D *S3_kp1_mass_total = new TH1D("S3_kp1_mass_total","",500,0.3,0.8);
 
    // Make array for all the 1D projections
    TH1D *h_projectionx_S1_sig[500];
@@ -21,6 +26,12 @@
    TH1D *h_projectionx_S2_back[500];
    TH1D *h_projectionx_S3_sig[500];
    TH1D *h_projectionx_S3_back[500];
+
+
+   // Rebinning values
+   Int_t S3_rebin_x=1;
+   Int_t S3_rebin_y=20;
+   Int_t S3_rebin_z=4;
 
    //////////////////////////////////////////////////////////////////////////////
    //// Take all the projections    ///////////////////////////////////////
@@ -49,15 +60,28 @@
    TH1D *S2_kp1_mass_total = (TH1D*)h_S2_Photon_Energy__Miss_Mass__Kaon_Mass->Project3D("z")->Clone("S2_kp1_mass_total");
 
 
-   h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->Rebin3D(1,100,5);
+   h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->Rebin3D(S3_rebin_x,S3_rebin_y,S3_rebin_z);
+   S3_photon_energy_total->Rebin(S3_rebin_x);
+   S3_miss_mass_total->Rebin(S3_rebin_y);
+   S3_kp1_mass_total->Rebin(S3_rebin_z);
+
+
 
    // Strangeness 3
-   // Photon energy
-   TH1D *S3_photon_energy_total = (TH1D*)h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->Project3D("x")->Clone("S3_photon_energy_total");
-   // missing mass of electron and 3 kaons
-   TH1D *S3_miss_mass_total = (TH1D*)h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->Project3D("y")->Clone("S3_miss_mass_total");
-   // Mass of kaon 1
-   TH1D *S3_kp1_mass_total = (TH1D*)h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->Project3D("z")->Clone("S3_kp1_mass_total");
+   for(int s3_x=1;s3_x < S3_photon_energy_total->GetNbinsX()+1; s3_x++){
+      for(int s3_y=1;s3_y < S3_miss_mass_total->GetNbinsX()+1; s3_y++){
+         for(int s3_z=1;s3_z < S3_kp1_mass_total->GetNbinsX()+1; s3_z++){
+
+            S3_photon_energy_total->SetBinContent(s3_x, S3_photon_energy_total->GetBinContent(s3_x) + h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->GetBinContent(s3_x, s3_y, s3_z));
+            S3_miss_mass_total->SetBinContent(s3_y, S3_miss_mass_total->GetBinContent(s3_y) + h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->GetBinContent(s3_x, s3_y, s3_z));
+            S3_kp1_mass_total->SetBinContent(s3_z, S3_kp1_mass_total->GetBinContent(s3_z) + h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->GetBinContent(s3_x, s3_y, s3_z));
+
+         }
+      }
+   }
+
+
+
 
    //////////////////////////////////////////////////////////////////////////////
    //// Creating and fitting functions    ///////////////////////////////////////
@@ -150,10 +174,10 @@
 
    // Strangeness 3 - kaon 1
    func1_S3->SetParameter(0,S3_kp1_mass_total->GetMaximum() / 2);
-   func1_S3->SetParameter(1,0.493);
+   func1_S3->FixParameter(1,0.493);
    func1_S3->SetParameter(2,0.01);
    func1_S3->SetParameter(3,S3_kp1_mass_total->GetMaximum() / 2);
-   func1_S3->SetParameter(4,0.493);
+   func1_S3->FixParameter(4,0.493);
    func1_S3->SetParameter(5,0.03);
    func1_S3->SetParameter(6,S1_kp1_mass_total->GetMaximum() / 4);
    func1_S3->SetParameter(7,0.345);
@@ -162,11 +186,11 @@
 
    // Setting parameter limits before fitting
    func1_S3->SetParLimits(0,S3_kp1_mass_total->GetMaximum() / 5,S3_kp1_mass_total->GetMaximum()); // amplitude for first gauss
-   func1_S3->SetParLimits(1,0.480,0.505); // mean for first gauss
-   func1_S3->SetParLimits(2,0.005,0.02); // sigma for first gauss
+   // func1_S3->SetParLimits(1,0.480,0.505); // mean for first gauss
+   func1_S3->SetParLimits(2,0.005,0.03); // sigma for first gauss
    func1_S3->SetParLimits(3,S3_kp1_mass_total->GetMaximum() / 5,S3_kp1_mass_total->GetMaximum()); // amplitude for second gauss
-   func1_S3->SetParLimits(4,0.480,0.505); // mean for second gauss
-   func1_S3->SetParLimits(5,0.005,0.04); // sigma for second gauss
+   // func1_S3->SetParLimits(4,0.480,0.505); // mean for second gauss
+   func1_S3->SetParLimits(5,0.01,0.04); // sigma for second gauss
    func1_S3->SetParLimits(7,0.1,0.38); // mean for background gauss
 
 
@@ -260,16 +284,17 @@
       h_projectionx_S1_sig[bin] = (TH1D*)h_S1_Photon_Energy__Miss_Mass__Kaon_Mass->ProjectionY("",0,h_S1_Photon_Energy__Miss_Mass__Kaon_Mass->GetNbinsX(),bin,bin)->Clone(h_projectionx_S1_sig_name.str().c_str());
       h_projectionx_S1_back[bin] = (TH1D*)h_S1_Photon_Energy__Miss_Mass__Kaon_Mass->ProjectionY("",0,h_S1_Photon_Energy__Miss_Mass__Kaon_Mass->GetNbinsX(),bin,bin)->Clone("h_projectionx_S1_back");
 
-      // h_projectionx_S1_sig[bin]->Sumw2();
+      h_projectionx_S1_sig[bin]->Sumw2();
       // h_projectionx_S1_back[bin]->Sumw2();
 
       // Make sure there are reasonable statistics in the projection
       if(h_projectionx_S1_sig[bin]->Integral() > 100){
 
          // Determine the scaling factor looking at the signal function
-         S1_Scaling_Factor_sig = func5_S1->Eval(S1_kp1_mass_total->GetBinCenter(bin)) / h_projectionx_S1_sig[bin]->Integral() ;
+         S1_Scaling_Factor_sig = func4_S1->Eval(S1_kp1_mass_total->GetBinCenter(bin)) / h_projectionx_S1_sig[bin]->Integral() ;
 
          // Determine the scaling factor looking at the background function
+         // S1_Scaling_Factor_back = func5_S1->Eval(S1_kp1_mass_total->GetBinCenter(bin)) / h_projectionx_S1_back[bin]->Integral();
          S1_Scaling_Factor_back = (func5_S1->Eval(S1_kp1_mass_total->GetBinCenter(bin)) -
          func4_S1->Eval(S1_kp1_mass_total->GetBinCenter(bin))) / h_projectionx_S1_back[bin]->Integral();
 
@@ -300,6 +325,7 @@
    // Subtract the background histogram from the signal
    h_projectionx_S1_sig[1]->Add(h_projectionx_S1_back[1], -1);
 
+   h_projectionx_S1_sig[1]->Scale((S1_kp1_mass_total->Integral() - (func5_S1->Integral(0.37,0.78) / S1_kp1_mass_total->GetBinWidth(1))) / h_projectionx_S1_sig[1]->Integral());
 
    //////////////////////////////////////////////////////////////////////////////
    // Strangeness 2 - kaon 1
@@ -315,14 +341,16 @@
       h_projectionx_S2_sig[bin] = (TH1D*)h_S2_Photon_Energy__Miss_Mass__Kaon_Mass->ProjectionY("",0,h_S2_Photon_Energy__Miss_Mass__Kaon_Mass->GetNbinsX(),bin,bin)->Clone(h_projectionx_S2_sig_name.str().c_str());
       h_projectionx_S2_back[bin] = (TH1D*)h_S2_Photon_Energy__Miss_Mass__Kaon_Mass->ProjectionY("",0,h_S2_Photon_Energy__Miss_Mass__Kaon_Mass->GetNbinsX(),bin,bin)->Clone("h_projectionx_S2_back");
 
+      h_projectionx_S2_sig[bin]->Sumw2();
 
       // Make sure there are reasonable statistics in the projection
       if(h_projectionx_S2_sig[bin]->Integral() > 100){
 
          // Determine the scaling factor looking at the signal function
-         S2_Scaling_Factor_sig = func5_S2->Eval(S2_kp1_mass_total->GetBinCenter(bin)) / h_projectionx_S2_sig[bin]->Integral() ;
+         S2_Scaling_Factor_sig = func4_S2->Eval(S2_kp1_mass_total->GetBinCenter(bin)) / h_projectionx_S2_sig[bin]->Integral() ;
 
          // Determine the scaling factor looking at the background function
+         // S2_Scaling_Factor_back = func5_S2->Eval(S2_kp1_mass_total->GetBinCenter(bin)) / h_projectionx_S2_back[bin]->Integral();
          S2_Scaling_Factor_back = (func5_S2->Eval(S2_kp1_mass_total->GetBinCenter(bin)) -
          func4_S2->Eval(S2_kp1_mass_total->GetBinCenter(bin))) / h_projectionx_S2_back[bin]->Integral();
 
@@ -353,10 +381,15 @@
    // Subtract the background histogram from the signal
    h_projectionx_S2_sig[1]->Add(h_projectionx_S2_back[1], -1);
 
+   h_projectionx_S2_sig[1]->Scale((S2_kp1_mass_total->Integral() - (func5_S2->Integral(0.37,0.78) / S2_kp1_mass_total->GetBinWidth(1))) / h_projectionx_S2_sig[1]->Integral());
+
 
    //////////////////////////////////////////////////////////////////////////////
    // Strangeness 3 - kaon 1
-
+   Int_t Integral_Individual=0;
+   Int_t Integral[300];
+   Double_t Errors;
+   Int_t counter=0;
    // Loop over the kaon mass bins (z-axis) and make projection of MM (y-axis)
    for(Int_t bin = 1; bin < S3_kp1_mass_total->GetNbinsX(); bin++){
 
@@ -368,14 +401,17 @@
       h_projectionx_S3_sig[bin] = (TH1D*)h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->ProjectionY("",0,h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->GetNbinsX(),bin,bin)->Clone(h_projectionx_S3_sig_name.str().c_str());
       h_projectionx_S3_back[bin] = (TH1D*)h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->ProjectionY("",0,h_S3_Photon_Energy__Miss_Mass__Kaon_Mass->GetNbinsX(),bin,bin)->Clone("h_projectionx_S3_back");
 
+      // h_projectionx_S3_sig[bin]->Sumw2();
 
       // Make sure there are reasonable statistics in the projection
-      if(h_projectionx_S3_sig[bin]->Integral() > 20){
+      if(h_projectionx_S3_sig[bin]->Integral() > 15){
+         counter++;
 
          // Determine the scaling factor looking at the signal function
-         S3_Scaling_Factor_sig = func5_S3->Eval(S3_kp1_mass_total->GetBinCenter(bin)) / h_projectionx_S3_sig[bin]->Integral() ;
+         S3_Scaling_Factor_sig = func4_S3->Eval(S3_kp1_mass_total->GetBinCenter(bin)) / h_projectionx_S3_sig[bin]->Integral() ;
 
          // Determine the scaling factor looking at the background function
+         // S3_Scaling_Factor_back = func5_S3->Eval(S3_kp1_mass_total->GetBinCenter(bin)) / h_projectionx_S3_back[bin]->Integral();
          S3_Scaling_Factor_back = (func5_S3->Eval(S3_kp1_mass_total->GetBinCenter(bin)) -
          func4_S3->Eval(S3_kp1_mass_total->GetBinCenter(bin))) / h_projectionx_S3_back[bin]->Integral();
 
@@ -396,6 +432,16 @@
       h_projectionx_S3_back[1]->Add(h_projectionx_S3_back[bin]);
    }
 
+
+   // // Setting the Errorss
+   // for(int l = 0; l < h_projectionx_S3_sig[1]->GetNbinsX(); l++){
+   //    if(h_projectionx_S3_sig[1]->GetBinContent(l) == 0) continue;
+   //    Errors = h_projectionx_S3_sig[1]->GetBinContent(l) * (sqrt(Integral[l]) / Integral[l]);
+   //    h_projectionx_S3_sig[1]->SetBinError(l,Errors);
+   //
+   // }
+
+
    // Determining the integral of signal and background where MM < 1, this is
    // done to ensure below threshold background is removed properly
    Double_t back_integral_S3 = h_projectionx_S3_back[1]->Integral(h_projectionx_S3_back[1]->FindBin(0), h_projectionx_S3_back[1]->FindBin(1));
@@ -403,9 +449,67 @@
    // Scale the background accordingly for low MM
    h_projectionx_S3_back[1]->Scale(sig_integral_S3 / back_integral_S3);
 
+   Double_t Scaling = S3_kp1_mass_total->Integral() / h_projectionx_S3_sig[1]->Integral();
+
+   h_projectionx_S3_sig[1]->Scale(Scaling);
+   h_projectionx_S3_back[1]->Scale(Scaling);
+
    // Subtract the background histogram from the signal
    h_projectionx_S3_sig[1]->Add(h_projectionx_S3_back[1], -1);
 
+   h_projectionx_S3_sig[1]->Scale((S3_kp1_mass_total->Integral() - (func5_S3->Integral(0.37,0.78) / S3_kp1_mass_total->GetBinWidth(1))) / h_projectionx_S3_sig[1]->Integral());
+
+   // Setting up styles
+   gROOT->Reset();
+   TStyle *plain  = new TStyle("Plain","Plain Style (no colors/fill areas)");
+
+   plain->SetCanvasBorderMode(0);
+   plain->SetPadBorderMode(0);
+   plain->SetPadColor(0);
+   plain->SetCanvasColor(0);
+   plain->SetTitleColor(0);
+   plain->SetStatColor(0);
+   plain->SetPalette(1);
+   gROOT->SetStyle("Plain");
+   gStyle->SetPalette(1);
+   gStyle->SetPalette(1);
+   gStyle->SetPadLeftMargin(0.15);
+   gStyle->SetPadBottomMargin(0.15);
+   //gStyle->SetTitleFontSize(0.15);
+   gStyle->SetOptStat(0);
+   gStyle->SetLineStyleString(13,"10 30");  // dotted
+   gStyle->SetLineStyleString(12,"40 40"); // dashed
+   gStyle->SetLineStyleString(14,"50 25 10 25");
+
+
+   h_projectionx_S1_sig[1]->SetLineWidth(3);
+   h_projectionx_S1_sig[1]->SetLineColor(4);
+   h_projectionx_S1_sig[1]->SetMarkerStyle(8);
+   h_projectionx_S1_sig[1]->SetMarkerSize(1);
+   h_projectionx_S1_sig[1]->SetMarkerColor(4);
+   h_projectionx_S1_sig[1]->GetXaxis()->SetTitleOffset(1.2);
+   h_projectionx_S1_sig[1]->GetYaxis()->SetTitle("Counts");
+
+   h_projectionx_S2_sig[1]->SetTitle("RGB S1 Background Subtracted Missing Mass");
+   // h_projectionx_S2_sig[1]->Rebin(2);
+   h_projectionx_S2_sig[1]->SetLineWidth(3);
+   h_projectionx_S2_sig[1]->SetLineColor(4);
+   h_projectionx_S2_sig[1]->SetMarkerStyle(8);
+   h_projectionx_S2_sig[1]->SetMarkerSize(1);
+   h_projectionx_S2_sig[1]->SetMarkerColor(4);
+   h_projectionx_S2_sig[1]->GetXaxis()->SetTitleOffset(1.2);
+   h_projectionx_S2_sig[1]->GetYaxis()->SetTitle("Counts");
+
+   h_projectionx_S3_sig[1]->SetTitle("Background Subtracted Missing Mass");
+   // h_projectionx_S3_sig[1]->Rebin(4);
+   // h_projectionx_S3_back[1]->Rebin(4);
+   h_projectionx_S3_sig[1]->SetLineWidth(3);
+   // h_projectionx_S3_sig[1]->SetLineColor(kRed);
+   h_projectionx_S3_sig[1]->SetMarkerStyle(8);
+   h_projectionx_S3_sig[1]->SetMarkerSize(1);
+   h_projectionx_S3_sig[1]->SetMarkerColor(4);
+   h_projectionx_S3_sig[1]->GetXaxis()->SetTitleOffset(1.2);
+   h_projectionx_S3_sig[1]->GetYaxis()->SetTitle("Counts");
 
    // Strangness 1
    func2_S1->SetLineColor(kBlue);
@@ -437,24 +541,35 @@
    // Cascade 1530 state
    auto *cascade_1530_line = new TLine(1.535,0,1.535,350);
 
+   // Threshold for S3
+   auto *S3_Hex = new TLine(1.737,0,1.737,40);
+   auto *S3_Mol = new TLine(1.969,0,1.969,40);
+   auto *S3_Threshold_line = new TLine(1.8154,0,1.8154,40);
+
    // Change colours of functions to made it clearer
    // Particle lines
    cascade_line->SetLineColor(kRed);
    cascade_1530_line->SetLineColor(kRed);
+   S3_Threshold_line->SetLineColor(kRed);
+   S3_Hex->SetLineWidth(3);
+   S3_Mol->SetLineWidth(3);
+   S3_Threshold_line->SetLineWidth(3);
+   S3_Hex->SetLineColor(kGreen);
+   S3_Mol->SetLineColor(kOrange);
 
 
-   auto *c1 = new TCanvas("c1","Strangeness 1 kaon 1 mass",800,800);
-   c1->cd();
-   S1_kp1_mass_total->Draw();
-   func1_S1->Draw("same");
-   func2_S1->Draw("same");
-   func3_S1->Draw("same");
-   func4_S1->Draw("same");
-   func5_S1->Draw("same");
+   // auto *c1 = new TCanvas("c1","Strangeness 1 kaon 1 mass",800,800);
+   // c1->cd();
+   // S1_kp1_mass_total->Draw();
+   // func1_S1->Draw("same");
+   // func2_S1->Draw("same");
+   // func3_S1->Draw("same");
+   // func4_S1->Draw("same");
+   // func5_S1->Draw("same");
 
-   auto *c2 = new TCanvas("c2","Strageness 1 Before background Subtraction",800,800);
-   c2->cd();
-   S1_miss_mass_total->Draw();
+   // auto *c2 = new TCanvas("c2","Strageness 1 Before background Subtraction",800,800);
+   // c2->cd();
+   // S1_miss_mass_total->Draw();
 
 
    auto *c3 = new TCanvas("c3","Strageness 1 After background Subtraction",800,800);
@@ -462,43 +577,46 @@
    h_projectionx_S1_sig[1]->Draw();
    l1->Draw("same");
 
-   auto *c4 = new TCanvas("c4","Strangeness 2 kaon 1 mass",800,800);
-   c4->cd();
-   S2_kp1_mass_total->Draw();
-   func1_S2->Draw("same");
-   func2_S2->Draw("same");
-   func3_S2->Draw("same");
-   func4_S2->Draw("same");
-   func5_S2->Draw("same");
+   // auto *c4 = new TCanvas("c4","Strangeness 2 kaon 1 mass",800,800);
+   // c4->cd();
+   // S2_kp1_mass_total->Draw();
+   // func1_S2->Draw("same");
+   // func2_S2->Draw("same");
+   // func3_S2->Draw("same");
+   // func4_S2->Draw("same");
+   // func5_S2->Draw("same");
 
    // auto *c5 = new TCanvas("c5","Strageness 2 Before background Subtraction",800,800);
    // c5->cd();
    // S2_miss_mass_total->Draw();
    //
    //
-   // auto *c6 = new TCanvas("c6","Strageness 2 After background Subtraction",800,800);
-   // c6->cd();
-   // h_projectionx_S2_sig[1]->Draw();
-   // l1->Draw("same");
-   // cascade_line->Draw("same");
-   // cascade_1530_line->Draw("same");
+   auto *c6 = new TCanvas("c6","Strageness 2 After background Subtraction",800,800);
+   c6->cd();
+   h_projectionx_S2_sig[1]->Draw();
+   l1->Draw("same");
+   cascade_line->Draw("same");
+   cascade_1530_line->Draw("same");
 
-   // auto *c7 = new TCanvas("c7","Strangeness 3 kaon 1 mass",800,800);
-   // c7->cd();
-   // S3_kp1_mass_total->Draw();
-   // func1_S3->Draw("same");
-   // func2_S3->Draw("same");
-   // func3_S3->Draw("same");
-   // func4_S3->Draw("same");
-   // func5_S3->Draw("same");
+   auto *c7 = new TCanvas("c7","Strangeness 3 kaon 1 mass",800,800);
+   c7->cd();
+   S3_kp1_mass_total->Draw();
+   func1_S3->Draw("same");
+   func2_S3->Draw("same");
+   func3_S3->Draw("same");
+   func4_S3->Draw("same");
+   func5_S3->Draw("same");
 
    // auto *c8 = new TCanvas("c8","Strageness 3 Before background Subtraction",800,800);
    // c8->cd();
    // S3_miss_mass_total->Draw();
    //
    //
-   // auto *c9 = new TCanvas("c9","Strageness 3 After background Subtraction",800,800);
-   // c9->cd();
-   // h_projectionx_S3_sig[1]->Draw();
-   // l1->Draw("same");
+   auto *c9 = new TCanvas("c9","Strageness 3 After background Subtraction",800,800);
+   c9->cd();
+   h_projectionx_S3_sig[1]->Draw();
+   l1->Draw("same");
+   S3_Hex->Draw("same");
+   S3_Mol->Draw("same");
+   S3_Threshold_line->Draw("same");
 }
